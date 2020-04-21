deb87bef8ce156f00bc3ecd29797de62cb988507
Alec Wong <aw685@cornell.edu>
2020-04-20 20:59:00 -0400

    Update explore

--- submissions/alec_wong/scripts/explore_1.R
+++ submissions/alec_wong/scripts/explore_1.R
@@ -4,6 +4,9 @@ library(dplyr)
 library(data.table)
 library(purrr)
 library(mapsapi)
+library(xml2)
+
+source("R/load_all_data.R")
 
 # Data import -- use predefined function. Train-test are combined but labeled.
 
@@ -24,3 +27,13 @@ data %>% lapply(FUN = function(x){
 # MSSubClass should be factorized.
 # Neighborhood is interesting, want to try SAR model. City is Ames, IA.
 
+api_key = Sys.getenv("gmaps_api_key")
+
+ames_geocode = mp_geocode("Ames, Iowa", key = api_key)
+mp_map(center = 'Ames, Iowa', zoom = 10, key = api_key, maptype = 'roadmap')
+
+
+ames_bb = osmdata::getbb("ames, iowa")
+q = osmdata::opq(bbox = ames_bb)
+
+ames_sf = q %>% osmdata_sf()
